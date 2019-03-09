86801559ddd9866789f972756064b4c12b8be369
Brenton Schulz <brenton@z2dsp.com>
2019-03-09 16:57:38 +1100

    Initial commit

---
+++ series.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+	int x1 = 0;
+	int x2 = 2;
+	int d;
+	int xn; // the current x value
+	int xnm1; // xn minus 1, the previous value
+
+	// d = ... COMPLETE THIS EXPRESSION
+
+	while ( /*condition*/ ) {
+		// Calculate current x value
+		xn /* = ... */;
+
+		// printf(" ");	// FILL ME IN	
+		
+		xnm1 = xn; // Make past value into present value
+	}	
+	return 0;
+}
