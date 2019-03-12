c63cd751b6486f5439e9eddff7a4bdbacc5539ac
Brenton Schulz <brenton@z2dsp.com>
2019-03-13 05:19:08 +1100

    Tweaked comments in series.c

--- series.c
+++ series.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main() {
-	int x1 = 0;
-	int x2 = 2;
-	int d;
-	int xn; // the current x value
-	int xnm1; // xn minus 1, the previous value
+	int x1 = 0;	// Initial value, might not need this variable
+	int x2 = 2;	// Another initial, might not need this either
+	int d;		// The difference between each sequence value
+	int xn; 	// the current x value
+	int xnm1; 	// xn minus 1, the previous value. Is this _really_ needed?
+	int n = 3; 	// Sequence entry number. Starts at 3 as n=1 and n=2 are
+			// already known above (x1 and x2)
 
 	// d = ... COMPLETE THIS EXPRESSION
 
